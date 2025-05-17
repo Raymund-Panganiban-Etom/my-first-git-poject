6ea0842c66a19aa813fc8e4d0d4442d59c8b07e9
Raymund Panganiban Etom <etom.raymund.panganiban@gmail.com>
2025-05-17 00:52:10 -0700

    Add if-statement C++

---
+++ If-else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int age;
+
+cout<<"Enter Age: ";
+cin>>age;
+
+if(age>=18){
+    cout<<"Your eligible to vote.";
+} else {
+    cout<<"Your not eligible to vote.";
+}
+system("pause");
+return 0;
+
+}
